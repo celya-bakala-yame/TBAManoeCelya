55667ff72ca7f47de5c65179e9fcdc280e6c174d
celya-bakala-yame <celya.bakala-yame@edu.esiee.fr>
2025-11-17 15:21:59 +0000

    Une première version

---
+++ item.py
@@ -0,0 +1,10 @@
+# item.py
+
+class Item:
+    def __init__(self, name, description, weight=0):
+        self.name = name
+        self.description = description
+        self.weight = weight
+
+    def __str__(self):
+        return f"{self.name} : {self.description} ({self.weight} kg)"
